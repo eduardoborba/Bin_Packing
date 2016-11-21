13bdb227f9290c3bfdda7000920e1465bdf99056
eduardoborba <eduardojoseborba@gmail.com>
2016-11-20 22:45:35 -0200

    Create Models

--- src/execute/Main.java
+++ src/execute/Main.java
@@ -1,10 +1,21 @@
 package execute;
 
+import model.Bin;
+import model.Package;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		Bin bin = new Bin();
+		for(int i = 0; i < 10; i++)
+		{
+			Package p = new Package((double)i/10);
+			bin.addItem(p);
+		}
+		for(Package p: bin.getItems())
+		{
+			System.out.println(p.getSize());
+		}
 	}
 
 }
